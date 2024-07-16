0700bc678c8a46b7282e060b550a8598e424f3c0
Martin Roelfs <martinroelfs@yahoo.com>
2024-07-16 11:35:12 +0200

    Fixed clipping issue

--- content/animations/config.py
+++ content/animations/config.py
@@ -5,8 +5,18 @@ options = dict(
     lineWidth=4,
     pointRadius=2.5,
     fontSize=3,
+    clip=10,
 )
 animated_options = dict(animate=1, **options)
-clrs = [0xff9900, 0xfed290, 0x009977, 0x000000]
+clrs = [
+    0x1F91CA, 0x57BFED,  # blue
+    0xF69B28, 0xFEC074,  # orange
+    0x0C3353, 0x1A5B8F,
+    0x34BAC4, 0x82DFE4,
+    0xBF0E1C, 0xEA575C,
+    0x640021, 0x941F4B,
+    0x000000
+]  # ECM colors
+# clrs = [0xff9900, 0xfed290, 0x009977, 0x000000]
 PLANE = alg3d.vector(e3=1)
 ORIGIN = alg3d.blades.e0.dual() | PLANE
